f351bf608896a0f3727a1ff3e41c8740e9a5cab9
Brayan <brayanc082006@gmail.com>
2025-03-09 14:00:36 -0500

    Promedio_de_tres_notas_Array
    Código terminado para sacar el promedio de tres notas

--- Promedio_de_tres_notas_Array.cpp
+++ Promedio_de_tres_notas_Array.cpp
@@ -5,20 +5,24 @@ using namespace std;
 
 int main()
 {
+	//Declarar variables
+	int Pro = 0;
+	
 	//Declarar array
-	int d1[3];
+	int N[3];
 	
 	//Entrada de valores en el vector
 	for(int i=0; i<=2; i++)
 	{
 		cout<<"Ingrese el valor de la nota #"<<(i+1)<<": ";
-		cin>>d1[i];
-	}
+		cin>>N[i];
+	}	cout<<endl; //Solo para saltar linea
 	
 	//Salida datos
 	for(int i=0; i<=2; i++)
 	{
-		cout<<"el valor de la nota #"<<(i+1)<<" es: "<<d1[i]<<endl;
-		cout<<"El promedio de las notas es: "<<
+		cout<<"el valor de la nota #"<<(i+1)<<" es: "<<N[i]<<endl;
 	}	
+		Pro = (N[0] + N[1] + N[2]) / 3;
+		cout<<"\nEl promedio de las notas es: "<<Pro<<endl;
 } 
