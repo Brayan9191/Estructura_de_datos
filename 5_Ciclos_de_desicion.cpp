89328018b611713db0de9a789c0092aa8836b5f9
Brayan <brayanc082006@gmail.com>
2025-03-11 20:14:49 -0500

    Update 5_Ciclos_de_desicion.cpp

--- 5_Ciclos_de_desicion.cpp
+++ 5_Ciclos_de_desicion.cpp
@@ -6,31 +6,31 @@ using namespace std;
 
 int main()
 {
-//	if(condicion)
-//	{
-//		codigo
-//		if(condicion) -> if anidado
-//	 } 
-//	else (condicion) 
-//	{
-// 		codigo
-//	}
+/*	if(condicion)
+	{
+		codigo
+		if(condicion) -> if anidado
+	 } 
+	else (condicion) 
+	{
+ 		codigo
+	}
 
-//	switch(opc)
-//	{
-//		case 1: codigo
-//			break;
-//		
-//		case 2: codigo
-//			break;
-//		
-//		case 3: codigo
-//			break;
-//
-//		default: codigo
-//			break;
-//	}
-	
+	switch(opc)
+	{
+		case 1: codigo
+			break;
+		
+		case 2: codigo
+			break;
+		
+		case 3: codigo
+			break;
+
+		default: codigo
+			break;
+	}
+*/	
 	getch();
 	return 0;
 }
